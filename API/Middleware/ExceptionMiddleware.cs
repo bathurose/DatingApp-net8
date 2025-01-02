15f9f3f0867d38464bff629cb651d03539c56bf7
unknown <tuannguyen@SM87.SMC.local>
2025-01-02 17:15:52 +0700

    update

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using API.Errors;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace API.Middleware
+{
+    public class ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger,
+        IHostEnvironment env)
+    {
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await next(httpContext);
+            }
+            catch (Exception ex) {
+                logger.LogError(ex, ex.Message); // print error to terminal
+                httpContext.Response.ContentType = "application/json";
+                httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var response = env.IsDevelopment() ? new ApiException(httpContext.Response.StatusCode, ex.Message, ex.StackTrace)
+                    : new ApiException(httpContext.Response.StatusCode, ex.Message, "Internal server error");
+
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                };
+
+                var json = JsonSerializer.Serialize(response, options);
+
+                await httpContext.Response.WriteAsync(json);
+            }
+        }
+    }
+}
